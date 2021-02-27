d97c635a7c0445b0276013e6795bb8ab2b783c4f
unknown <avinashupadhya99@gmail.com>
2021-02-27 21:27:47 +0530

    Implemented login using flask-wtf. Fixed static files location

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,18 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect
+from app.forms import LoginForm
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}'.format(form.username.data))
+        return redirect('/')
+    return render_template('login.html', title='Sign In', form=form)
 
 @app.route('/register')
 def register():
