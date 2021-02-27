895235ddcd64b759004be37cdc995753c04214a8
unknown <avinashupadhya99@gmail.com>
2021-02-27 22:53:18 +0530

    Implemented login logic

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from app import app
 from flask import render_template, flash, redirect
+from flask_login import current_user, login_user, logout_user, login_required
 from app.forms import LoginForm
 
 @app.route('/')
@@ -8,16 +9,31 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}'.format(form.username.data))
-        return redirect(url_for('index'))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
 
 @app.route('/register')
 def register():
     return render_template('register.html')
 
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
 @app.route('/profile')
+@login_required
 def user_profile():
     return render_template('user-profile.html')
